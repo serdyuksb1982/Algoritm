1c839c4fde74f3aa41fbdfc523d94990ae35e9c9
Sergey Serdyuk <serdyuksb@gmail.com>
2023-03-07 10:12:13 +0300

    correct merge sort.

--- src/ru/serdyuk/mergesort/MergeSort.java
+++ src/ru/serdyuk/mergesort/MergeSort.java
@@ -3,6 +3,15 @@ package ru.serdyuk.mergesort;
 import java.util.Arrays;
 
 public class MergeSort {
+
+    public static void main(String[] args) {
+        int[] list = new int[(int) Math.round(Math.random() * Byte.MAX_VALUE)];
+        for (int i = 0; i < list.length; i++) {
+            list[i] = (int) Math.round(Math.random() * Byte.MAX_VALUE);
+        }
+        System.out.println(Arrays.toString(list));
+        System.out.println(Arrays.toString(mergeSort(list)));
+    }
     public static int[] mergeSort(int[] src) {
         if(src.length <= 1) return src;
         int[] left = Arrays.copyOfRange(src, 0, src.length / 2);
@@ -10,18 +19,20 @@ public class MergeSort {
         return merge(mergeSort(left), mergeSort(right));
     }
 
-    private static int[] merge(int[] left, int[] right) {
-        int[] result = new int[left.length + right.length];
-        int resIn = 0, leftIn = 0, rightIn = 0;
-        while (leftIn < left.length && rightIn < right.length) {
-            if (left[leftIn] < right[rightIn]) {
-                result[resIn++] = left[leftIn++];
-            } else result[resIn++] = right[rightIn++];
+    private static int[] merge(int[] leftArray, int[] rightArray) {
+        int[] result = new int[leftArray.length + rightArray.length];
+        int resultIndex = 0;
+        int leftArrayIndex = 0;
+        int rightArrayIndex = 0;
+        while (leftArrayIndex < leftArray.length && rightArrayIndex < rightArray.length) {
+            if (leftArray[leftArrayIndex] < rightArray[rightArrayIndex]) {
+                result[resultIndex++] = leftArray[leftArrayIndex++];
+            } else result[resultIndex++] = rightArray[rightArrayIndex++];
         }
-        while (resIn < result.length) {
-            if (leftIn != left.length) {
-                result[resIn++] = left[leftIn++];
-            } else result[resIn++] = right[rightIn++];
+        while (resultIndex < result.length) {
+            if (leftArrayIndex != leftArray.length) {
+                result[resultIndex++] = leftArray[leftArrayIndex++];
+            } else result[resultIndex++] = rightArray[rightArrayIndex++];
         }
         return result;
     }
